0b482f3e752e7fd93f1645c2a7112538c7425f68
bsijl <116880306+bsijl@users.noreply.github.com>
2024-02-13 18:00:38 +0800

    Update chat.js

--- HttpClient/wwwroot/js/chat.js
+++ HttpClient/wwwroot/js/chat.js
@@ -1,50 +1,63 @@
-﻿// The following sample code uses modern ECMAScript 6 features
-// that aren't supported in Internet Explorer 11.
-// To convert the sample for environments that do not support ECMAScript 6,
-// such as Internet Explorer 11, use a transpiler such as
-// Babel at http://babeljs.io/.
-//
-// See Es5-chat.js for a Babel transpiled version of the following code:
-//const connection = new signalR.HubConnectionBuilder()
-//    .withUrl("http://localhost:8082/ChatHub")
-//    .build();
+//以下示例代码使用了现代的ECMAScript 6功能，这些功能在Internet Explorer 11中不受支持。
+//为了将示例转换为不支持ECMAScript 6的环境（例如Internet Explorer 11），请使用转译器，如http://babeljs.io/上的Babel。
+//请参阅Es5 - chat.js，其中包含了上述代码的Babel转译版本。
 var connection = $.hubConnection("http://localhost:8082/signalr", { useDefaultPath: false });
+var proxy = connection.createHubProxy('ChatHub');
 window.onload = () => {
-    var proxy = connection.createHubProxy('ChatHub');
-    proxy.on('SendMessage', function (name, message) {
-        console.log(name + ' ' + message);
+  
+    proxy.on('SendMessage', function (opt, message) {
+        switch (opt) {
+            case "HtmlClientOK":
+                let winInfo = {
+                    title: document.title,
+                    rect: getLocationInfo("video")
+                }
+                $("#message").html("服务器连接成功并显示窗口");
+                proxy.invoke('SendMessage', 'HtmlClinetOK', JSON.stringify(winInfo))
+                break;
+            default:
+        }
     });
     // 建立链接
     connection.start().done(function () {
-        //$('#sendmessage').click(function () {
-        console.log('服务器连接成功');
-        let winInfo = {
-            title: document.title,
-            rect: getLocationInfo("video")
-        }
-        proxy.invoke('SendMessage', 'HtmlClinetOK',JSON.stringify(winInfo))
-        //});
+        //这里的优先级没有 proxy.on 高！信息都返回了，才运行到这里
+        //proxy.invoke('SendMessage', 'HtmlClinetOK', JSON.stringify(winInfo))
     })
         .fail(function () {
-            console.log('服务器连接失败');
+            $("#message").html("服务器连接失败");
         });
+    connection.error((error) => {//与服务器断开，等等吧，只要出错了
+        $("#message").html("服务器连接断开");
+    })
+
+    connection.reconnected(() => {//没测出来--页面在没有关闭的情况下，HttpServer重启后，会重新连接
+        $("#message").html("服务器重新连接上服务器");
+    })
+
+    connection.disconnected(() => {//与服务器连接失败，优先级低于.fail
+        console.log('disconnected')
+    })
+
+    connection.received((data) => {//连接后最先收到消息，数组形式的
+        console.log('received')
+        console.log(data)
+    })
+
+    connection.connectionSlow(() => {//没测出来--断过一次，好像是超时--英文含义是连接速度慢
+        $("#message").html("与服务器的通信有点慢啊");
+    })
 };
 
+
 document.getElementById("sendButton1").addEventListener("click", event => {
-    connection.invoke("SendMessage", "HideWin", "video").catch(err => console.error(err));
-    event.preventDefault();
+    proxy.invoke("SendMessage", "HideWin", "");
 });
 document.getElementById("sendButton2").addEventListener("click", event => {
-    var rect = getLocationInfo("video") 
-   
-    let videoRect = {
-        'rect': rect,
-    }
-    connection.invoke("SendMessage", "ShowWin", JSON.stringify(rect)).catch(err => console.error(err));
-    event.preventDefault();
+ 
+    proxy.invoke("SendMessage", "ShowWin", "");
 });
-// 获取位置信息
-function getLocationInfo (id) {
+// 获取位置信息--这里的不准确，该怎么调整？但是这段代码在另外一个程序里运行没问题
+function getLocationInfo(id) {
     const elt = document.getElementById(id);
     var rect = elt.getBoundingClientRect()
     var ua = navigator.userAgent;
@@ -59,8 +72,8 @@ function getLocationInfo (id) {
         };
     } else {
         return {
-            left: Math.floor(elt.getBoundingClientRect().left * getZoomScale()+6),
-            top: Math.floor(elt.getBoundingClientRect().top * getZoomScale()) + (window.outerHeight - window.innerHeight)+15,
+            left: Math.floor(elt.getBoundingClientRect().left * getZoomScale() + 6),
+            top: Math.floor(elt.getBoundingClientRect().top * getZoomScale()) + (window.outerHeight - window.innerHeight) + 15,
             width: Math.floor(elt.getBoundingClientRect().width * getZoomScale()),
             height: Math.floor(elt.getBoundingClientRect().height * getZoomScale()),
             screenLeft: Math.floor(window.screenLeft)
